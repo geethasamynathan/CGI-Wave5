582f753081d85a319a9dd3dbe4df0ea80357c638
geethasamynathan <geethasamynathan2011@gmail.com>
2021-09-14 16:43:41 +0530

    web api first demo

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI_Demo1.Models;
+
+namespace WebAPI_Demo1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [Route("employees")]
+        [HttpGet]
+        public IEnumerable<Employee> GetEmployees()
+        {
+            EmployeeRepository employeeRepository = new EmployeeRepository();
+            return employeeRepository.GetEmployees();
+        }
+    }
+}
---
+++ Models/EmployeeRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_Demo1.Models
+{
+    public class EmployeeRepository
+    {
+        static List<Employee> Employees= new List<Employee>() ;
+     public EmployeeRepository()
+    {
+            Employee emp1 = new Employee() { Id = 1, Name = "pam" };
+            Employee emp2 = new Employee() { Id = 2, Name = "Sam" };
+            Employee emp3 = new Employee() { Id = 3, Name = "Tom" };
+            Employees.Add(emp1);
+            Employees.Add(emp2);
+            Employees.Add(emp3);
+        }
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return Employees;
+        }
+        public Employee GetEmployeeById(int id)
+        {
+            return Employees.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}

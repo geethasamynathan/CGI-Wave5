5c43ef0dc0d97b3ea3d359e17b61c6c95e81a3fa
geethasamynathan <geethasamynathan2011@gmail.com>
2021-09-17 10:59:05 +0530

    Add files via upload

---
+++ EFCOreDEmo1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EFCore_Demo1
+{
+    class Program
+    {
+       static SchoolContext context;
+        static void Main(string[] args)
+        {
+          //  UpdateStudent();
+            GetAllStudents();
+            Console.WriteLine("\n\n Enter the id the student to delete");
+            int id = Convert.ToInt32(Console.ReadLine());
+            DeleteStudent(id);
+            Console.WriteLine(" \n\n After deleting the given student our records are below");
+            GetAllStudents();
+        }
+      public void  AddNewStudent()
+        {
+            context = new SchoolContext();
+            Student stu = new Student();
+            stu.Name = "Geetha";
+            context.Students.Add(stu);
+            context.SaveChanges();
+        }
+        public static void GetAllStudents()
+        {
+            context = new SchoolContext();
+               List<Student> studentList = context.Students.ToList();
+          
+            foreach (var item in studentList)
+            {
+                Console.WriteLine($" Id : {item.StudentId} \t Name :{item.Name}");
+            }
+        }
+        public static void UpdateStudent()
+        {
+            context = new SchoolContext();
+            Student student = context.Students.FirstOrDefault(s => s.StudentId == 1);
+            student.Name = "Yamuna";
+            context.SaveChanges();
+        }
+        public static void DeleteStudent(int id)
+        {
+            context = new SchoolContext();
+            Student student = context.Students.FirstOrDefault(s => s.StudentId == id);
+               context.Students.Remove(student);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ EFCOreDEmo1/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_Demo1
+{
+    class SchoolContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SchoolDB;Integrated Security=True");
+        }
+            public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        }
+}
